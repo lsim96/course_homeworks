2801b35bf04376981f2fa8e4ecb8c35061336469
lsim96 <leon.simonoskiii@gmail.com>
2024-11-30 02:54:13 +0100

    Two ways

--- Homework08/script.js
+++ Homework08/script.js
@@ -1,6 +1,14 @@
+//  First way
 let singlePhone = 119.95;
 let taxPerPhone = 5.9975;
 let bundleOfPhones = 30;
 
 let totalAmount = bundleOfPhones * (singlePhone + taxPerPhone);
 console.log(totalAmount);
+// Second Way
+let phonePrice = 119.95;
+let phoneTax = phonePrice * 0.05;
+let bulkOfPhones = 30;
+
+let fullAmount = bulkOfPhones * (phonePrice + phoneTax);
+console.log(fullAmount);
