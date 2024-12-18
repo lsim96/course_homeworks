8cc56b45994ae0540d2c0964b17d71e10ed9bfcb
lsim96 <leon.simonoskiii@gmail.com>
2024-12-18 15:36:06 +0100

    Updates

--- Homework12/homework1/script.js
+++ Homework12/homework1/script.js
@@ -1,9 +1,5 @@
-function tellStory() {
-  let array = ["Leon", "good", "listening to music"];
-  console.log(
-    `This is ${array[0]}. ${array[0]} is a nice person. Today he is in a ${array[1]} mood. He is ${array[2]} all day. The end.`
-  );
-  return array;
+function tellStory(array) {
+  return `This is ${array[0]}. ${array[0]} is a nice person. Today he is in a ${array[1]} mood. He is ${array[2]} all day. The end}`;
 }
 
-tellStory();
+console.log(tellStory(["Leon", "good", "listening to music"]));
--- Homework12/homework3/script.js
+++ Homework12/homework3/script.js
@@ -1,7 +1,18 @@
-function arrayOfStrings() {
-  let array = ["Hello", "there", "students", "of", "SEDC", "!"];
-  console.log(array[0], array[1], array[2], array[3], array[4], array[5]);
-  return array;
+// function arrayOfStrings() {
+//   let array = ["Hello", "there", "students", "of", "SEDC", "!"];
+//   console.log(array[0], array[1], array[2], array[3], array[4], array[5]);
+//   return array;
+// }
+
+// arrayOfStrings();
+
+function arrayOfStrings(arr) {
+  let message = "";
+  for (let i = 0; i < arr.length; i++) {
+    message = message + arr[i] + " ";
+  }
+  return message;
 }
 
-arrayOfStrings();
+let array = ["Hello", "there", "students", "of", "SEDC", "!"];
+console.log(arrayOfStrings(array));
