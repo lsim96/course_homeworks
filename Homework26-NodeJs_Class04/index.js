ccaea38d0e2a17f432d3bd025d2da80f67258976
lsim96 <leon.simonoskiii@gmail.com>
2025-03-12 12:07:05 +0100

    Updated homework

--- Homework26-NodeJs_Class04/index.js
+++ Homework26-NodeJs_Class04/index.js
@@ -15,21 +15,21 @@ studentEmitter.on("studentAdded", (studentName) => {
 });
 
 const server = http.createServer((req, res) => {
-  //   if (req.url === "/") {
-  //     res.writeHead(200, { "content-type": "text/html" });
-  //     res.end(`<h1>Welcome to the main page!</h1>`);
-  //   } else if (req.url === "/student") {
-  //     res.writeHead(200, { "content-type": "text/html" });
-  //     res.end(
-  //       `<p>Name: Leonid</p>
-  //         <p>Lastname: Simonoski</p>
-  //         <p>Academy: Qinshift Academy</p>
-  //         <p>Subject: NodeJs-Basic</p>`
-  //     );}
+  if (req.url === "/") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.end(`<h1>Welcome to the main page!</h1>`);
+  } else if (req.url === "/student") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.end(
+      `<p>Name: Leonid</p>
+          <p>Lastname: Simonoski</p>
+          <p>Academy: Qinshift Academy</p>
+          <p>Subject: NodeJs-Basic</p>`
+    );
+  }
 
-  //I COMMENTED OUT THE FIRST PART, SO IT WONT GET CONFUSING!!
-
-  if (req.url === "/add_student" && req.method === "GET") {
+  // I COMMENTED OUT THE FIRST PART, SO IT WONT GET CONFUSING!!
+  else if (req.url === "/add_student" && req.method === "GET") {
     res.writeHead(200, { "content-type": "text/html" });
     res.end(`<form action="/all_students" method="POST">
             <label for="studentName">Enter student name:</label><br>
@@ -60,6 +60,6 @@ const server = http.createServer((req, res) => {
   }
 });
 
-server.listen(3000, () => {
+server.listen(3002, () => {
   console.log("Server is running at http://localhost:3000");
 });
